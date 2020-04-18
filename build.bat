6bed3ca2709590e058457ea9c26d6f05bc85353b
tretrauit <teppyboydev@gmail.com>
2020-04-18 11:18:55 +0700

    Added Simple build script for Windows (Using OCRA)

---
+++ build.bat
@@ -0,0 +1,10 @@
+@echo off
+Title Building SentenceBackwarder for Windows
+echo Installing OCRA
+gem install ocra
+echo Should be installed, building for Windows
+ocra --quiet --no-dep-run --console SentenceBackwarder.rb
+echo Build done, you can run SentenceBackwarder.exe now.
+timeout 3 > NUL
+echo Exiting.
+exit
