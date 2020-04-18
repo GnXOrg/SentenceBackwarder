e5dd883821e253f5d535718692bf2f6dc9626634
tretrauit <teppyboydev@gmail.com>
2020-04-18 10:40:21 +0700

    Update

--- SentenceBackwarder.rb
+++ SentenceBackwarder.rb
@@ -1,2 +1,21 @@
 # Add the file
-puts "Test"
+puts "Welcome to SentenceBackwarder (v20.04.18)"
+def SentenceBackwarder()
+    puts "Type a sentence below to backward it"
+    sentence = gets.chomp
+    if (sentence.length <= 1)
+        puts "Length must be 2 or higher to backward!"
+    else
+        begin
+            puts "Backwarded sentence: #{sentence.reverse}"
+            puts "Do you want to backward more sentence? (Y/N)"
+            # Incomplete
+        rescue => exception
+            
+        end
+    end
+end
+
+def askForContinue()
+# Placeholder
+end    
