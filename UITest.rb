d82564a7ebf7f0ae9f52e2c8358beb9849b6882e
tretrauit <teppyboydev@gmail.com>
2020-04-18 14:16:04 +0700

    aaa

--- UITest.rb
+++ UITest.rb
@@ -1,6 +1,6 @@
 Shoes.app :title => "SentenceBackwarder", :width => 640, :height => 480 do
   @paragraph = para "Type a sentence to backwards it:"
-  @box = edit_box :width => 100, :height => 20, :text =>''
+  @box = edit_box :width => 100, :height => 20, :left => 235, :top => 3 , :text =>''
   stack(margin: 8) {
     @button = button("Reverse it!") do
       @outbox.text = @box.text.reverse
