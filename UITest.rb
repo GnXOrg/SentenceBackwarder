4465e0fa6059a5b2d4a1461358491fa61bebaa46
tretrauit <teppyboydev@gmail.com>
2020-04-18 13:41:30 +0700

    Update UITest.rb

--- UITest.rb
+++ UITest.rb
@@ -1,4 +1,8 @@
-Shoes.app do
-    background "#DFA"
-    para "this is a test"
+Shoes.app :title => "SentenceBackwarder", :width => 640, :height => 480 do
+  @paragraph = para "Type a sentence to backwards it:"
+  @box = edit_box :width => 100, :height => 20, :text =>''
+  @button = button("Reverse it!") do
+    @outbox.text = @box.text.reverse
   end
+  @outbox = edit_box :width => 100, :height => 20, :text =>''
+end
